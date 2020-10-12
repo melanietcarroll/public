7621d32437a87222bd9e9bd8570d752a54eb0cfd
Melanie Carroll <melanietcarroll@gmail.com>
2020-10-11 20:08:51 -0500

    added methods exercises

--- Classwork/M1/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/whiles/BewareTheKracken.java
+++ Classwork/M1/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/whiles/BewareTheKracken.java
@@ -13,17 +13,13 @@ import java.util.Random;
 public class BewareTheKracken {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        Random random = new Random();
-        int randomNumber = random.nextInt(2);
-        
-        
-        
+                  
         System.out.println("Already, get those flippers and wetsuit on - we're going diving!");
         System.out.println("Here we goooOOooOooo.....! *SPLASH*");
         
         int depthDivedInFt = 0;
         while(depthDivedInFt < 36200){
-            System.out.println("So far, we've swam " + depthDivedInFt + " feet");
+            System.out.println("So far, we've swam " + depthDivedInFt + " feet" + " and there goes a " + chooseFish());
           
             
             if(depthDivedInFt >=1000){
@@ -47,4 +43,14 @@ public class BewareTheKracken {
         System.out.println("I bet we can do better next time!");
     
     }
+    public static String chooseFish(){
+        Random random = new Random();
+ 
+        String[] fish = {"tuna", "shark", "clown fish"};
+        
+        return fish[random.nextInt(fish.length)];
+        
+    }
+    //if we change the while loop to true, the loop would still execute the same until the condition turned false
+    //then the loop would exit
 }
