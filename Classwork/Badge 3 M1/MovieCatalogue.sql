38dfbc16966354ded3fbd6895d5d7465481657c3
Melanie Carroll <melanietcarroll@gmail.com>
2020-12-16 05:59:51 -0600

    DDL exercise SQL

--- Classwork/Badge 3 M1/MovieCatalogue.sql
+++ Classwork/Badge 3 M1/MovieCatalogue.sql
@@ -1,3 +1,4 @@
+DROP DATABASE IF EXISTS MovieCatalogue;
 CREATE DATABASE IF NOT EXISTS MovieCatalogue;
 
 USE MovieCatalogue;
@@ -29,7 +30,7 @@ CREATE TABLE Actor (
 CREATE TABLE Movie (
     MovieID INT PRIMARY KEY AUTO_INCREMENT,
     RatingID INT NOT NULL,
-    DirectorID INT NOT NULL,
+    DirectorID INT,
     GenreID INT NOT NULL,
     Title VARCHAR(128) NOT NULL,
     ReleaseDate DATE,
@@ -51,3 +52,95 @@ CREATE TABLE CastMembers (
     FOREIGN KEY fk_CastMembers_Movie (MovieID)
         REFERENCES Movie (MovieID)
 );
+
+INSERT INTO Actor (ActorID, FirstName, LastName, BirthDate) VALUES
+    (1, 'Bill','Murray', '1950/09/21'),
+    (2, 'Dan','Aykroyd', '1952/07/01'),
+    (3, 'John','Candy', '1950/10/31'),
+    (4, 'Steve','Martin', NULL),
+    (5, 'Sylvester','Stallone', NULL);
+    
+SELECT * FROM Actor;
+
+INSERT INTO Director (DirectorID, FirstName, LastName, BirthDate) VALUES
+    (1, 'Ivan','Reitman', '1946/10/27'),
+    (2, 'Ted','Kotcheff', NULL);
+    
+SELECT * FROM Director;
+
+INSERT INTO Rating (RatingID, RatingName) VALUES
+    (1,'G'),
+    (2,'PG'),
+    (3,'PG-13'),
+    (4, 'R');
+    
+SELECT * FROM Rating;
+
+INSERT INTO Genre (GenreID, GenreName) VALUES
+    (1,'Action'),
+    (2,'Comedy'),
+    (3,'Drama'),
+    (4, 'Horror');
+    
+SELECT * FROM Genre;
+
+INSERT INTO Movie (RatingID, DirectorID, GenreID, Title, ReleaseDate) VALUES
+    (4, 2, 1,  'Rambo: First Blood','1982/10/22'),
+    (4, NULL, 2, 'Planes, Trains & Automobiles', '1987/11/25'),
+    (2, 1, 2, 'Ghostbusters',NULL),
+    (2, NULL, 2, 'The Great Outdoors', '1988/06/17');
+ 
+ SELECT * FROM Movie;
+ 
+ INSERT INTO CastMembers (Role, ActorID, MovieID) VALUES
+    ('John Rambo', 5, 1),
+    ('Neil Page', 4, 2),
+    ('Del Griffith', 3, 2),
+    ('Dr. Peter Venkman', 1, 3),
+    ('Dr. Raymond Stanz', 2, 3),
+    ('Roman Craig', 2, 4),
+    ('Chet Ripley', 3, 4);
+    
+SELECT * FROM CastMembers;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Movie SET
+    Title = 'Ghostbusters (1984)',
+    ReleaseDate = '1984/06/08'
+WHERE MovieID = 3;
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM Movie;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Genre SET
+    GenreName  = 'Action/Adventure'
+WHERE GenreID = 1;
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM Genre;
+
+DELETE FROM CastMembers
+WHERE MovieID = 1;
+
+DELETE FROM Movie
+WHERE MovieID = 1;
+
+SELECT * FROM Movie;
+
+ALTER TABLE Actor
+    ADD COLUMN (
+        DateOfDeath DATE
+    );
+ 
+SELECT * FROM Actor;
+ 
+SET SQL_SAFE_UPDATES = 0;
+    
+UPDATE Actor SET
+    DateOfDeath = '1994/03/04'
+WHERE ActorID = 3;
+
+SET SQL_SAFE_UPDATES = 1;
+
+SELECT * FROM Actor;
--- Classwork/Badge 3 M1/trackit_schema.sql
+++ Classwork/Badge 3 M1/trackit_schema.sql
@@ -1,57 +1,4 @@
-CREATE DATABASE IF NOT EXISTS TrackIt;
--- Note the USE! It's important.
-USE TrackIt;
 
-CREATE TABLE Task (
-    TaskId INT PRIMARY KEY AUTO_INCREMENT,
-    Title VARCHAR(100) NOT NULL,
-    Details TEXT NULL,
-    DueDate DATE NOT NULL,
-    EstimatedHours DECIMAL(5, 2) NULL
-);
-
-CREATE TABLE Project (
-    ProjectId CHAR(50) PRIMARY KEY,
-    `Name` VARCHAR(100) NOT NULL,
-    Summary VARCHAR(2000) NULL,
-    DueDate DATE NOT NULL,
-    IsActive BOOL NOT NULL DEFAULT 1
-);
-
-ALTER TABLE Task
-    ADD COLUMN (
-        ProjectId CHAR(50) NOT NULL
-    ),
-    ADD CONSTRAINT fk_Task_Project
-        FOREIGN KEY (ProjectId) 
-        REFERENCES Project(ProjectId);
-        
-CREATE TABLE Worker (
-    WorkerId INT PRIMARY KEY AUTO_INCREMENT,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    ProjectId CHAR(50) NOT NULL,
-    FOREIGN KEY fk_Worker_Project (ProjectId)
-        REFERENCES Project(ProjectId)	
-);       
-
-ALTER TABLE Worker
-    DROP FOREIGN KEY fk_Worker_Project,
-    DROP COLUMN ProjectId;
-    
-ALTER TABLE Worker
-    MODIFY COLUMN FirstName VARCHAR(50) NOT NULL,
-    MODIFY COLUMN LastName VARCHAR(50) NOT NULL;  
-    
-CREATE TABLE ProjectWorker (
-    ProjectId CHAR(50) NOT NULL,
-    WorkerId INT NOT NULL,
-    PRIMARY KEY pk_ProjectWorker (ProjectId, WorkerId),
-    FOREIGN KEY fk_ProjectWorker_Project (ProjectId)
-        REFERENCES Project(ProjectId),
-    FOREIGN KEY fk_ProjectWorker_Worker (WorkerId)
-        REFERENCES Worker(WorkerId)
-);    
 
 -- This approach destroys the existing database and starts from scratch each time you run it.
 -- It's good for new development, but won't work for existing
@@ -98,3 +45,33 @@ CREATE TABLE Task (
     FOREIGN KEY fk_Task_ProjectWorker (ProjectId, WorkerId)
         REFERENCES ProjectWorker(ProjectId, WorkerId)
 );
+
+INSERT INTO Worker (WorkerId, FirstName, LastName)
+    VALUES (1, 'Rosemonde', 'Featherbie');
+    
+INSERT INTO Worker (FirstName, LastName)
+    VALUES ('Kingsly', 'Besantie');    
+    
+INSERT INTO Worker (FirstName, LastName) VALUES
+    ('Goldi','Pilipets'),
+    ('Dorey','Rulf'),
+    ('Panchito','Ashtonhurst');
+    
+SELECT * FROM Worker;
+INSERT INTO Worker (WorkerId, FirstName, LastName)
+    VALUES (50, 'Valentino', 'Newvill');
+    
+INSERT INTO Project (ProjectId, `Name`, DueDate)
+    VALUES ('db-milestone', 'Database Material', '2018-12-31'); 
+    
+INSERT INTO ProjectWorker (ProjectId, WorkerId)
+    VALUES ('db-milestone', 2);
+    
+INSERT INTO Project (ProjectId, `Name`, DueDate)
+	VALUES ('kitchen', 'Kitchen Remodel', '2025-07-15'); 
+    
+INSERT INTO ProjectWorker (ProjectId, WorkerId) VALUES 
+    ('db-milestone', 1), -- Rosemonde, Database
+    ('kitchen', 2),      -- Kingsly, Kitchen
+    ('db-milestone', 3), -- Goldi, Database
+    ('db-milestone', 4); -- Dorey, Database
