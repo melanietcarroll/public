1d285020497862c17c56623bd3e05b541bb2ef28
Melanie Carroll <melanietcarroll@gmail.com>
2020-12-16 19:48:22 -0600

    Exercise Aggregate Queries SQL

--- Classwork/Badge 3 M1/trackit-schema-and-data.sql
+++ Classwork/Badge 3 M1/trackit-schema-and-data.sql
@@ -1054,3 +1054,191 @@ FROM Worker w
 CROSS JOIN Project p
 WHERE w.WorkerId = 1
 AND p.ProjectId NOT LIKE 'game-%';
+
+SELECT
+    w.FirstName,
+    w.LastName,
+    p.Name ProjectName
+FROM Worker w
+INNER JOIN ProjectWorker pw ON w.WorkerId = pw.WorkerId
+INNER JOIN Project p ON pw.ProjectId = p.ProjectId
+ORDER BY w.LastName ASC;
+
+SELECT
+    w.FirstName,
+    w.LastName,
+    p.Name ProjectName
+FROM Worker w
+INNER JOIN ProjectWorker pw ON w.WorkerId = pw.WorkerId
+INNER JOIN Project p ON pw.ProjectId = p.ProjectId
+ORDER BY w.LastName ASC, p.Name ASC;
+
+SELECT
+    w.FirstName,
+    w.LastName,
+    p.Name ProjectName
+FROM Worker w
+INNER JOIN ProjectWorker pw ON w.WorkerId = pw.WorkerId
+INNER JOIN Project p ON pw.ProjectId = p.ProjectId
+ORDER BY w.LastName DESC, p.Name ASC;
+
+SELECT
+    t.Title,
+    s.Name StatusName
+FROM Task t
+LEFT OUTER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+ORDER BY s.Name ASC;
+
+-- Results are sorted non-null to null, then by TaskStatus.Name.
+-- That puts NULL values last.
+SELECT
+    t.Title,
+    s.Name StatusName
+FROM Task t
+LEFT OUTER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+ORDER BY ISNULL(s.Name), s.Name ASC;
+
+SELECT *
+FROM Worker
+ORDER BY LastName DESC
+LIMIT 0, 10;
+
+SELECT *
+FROM Worker
+ORDER BY LastName DESC
+LIMIT 10, 10;
+
+SELECT *
+FROM Worker
+ORDER BY LastName DESC
+LIMIT 200, 10;
+
+-- Skip the first 100 records and show the next 25.
+SELECT
+    w.FirstName,
+    w.LastName,
+    p.Name ProjectName
+FROM Worker w
+INNER JOIN ProjectWorker pw ON w.WorkerId = pw.WorkerId
+INNER JOIN Project p ON pw.ProjectId = p.ProjectId
+ORDER BY w.LastName DESC, p.Name ASC
+LIMIT 100, 25;
+
+USE TrackIt;
+
+-- Count TaskIds, 543 values
+SELECT COUNT(TaskId)
+FROM Task;
+
+-- Count everything, 543 values
+SELECT COUNT(*)
+FROM Task;
+
+SELECT
+    COUNT(t.TaskId)
+FROM Task t
+INNER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+WHERE s.IsResolved = 1;
+
+SELECT
+    IFNULL(s.Name, '[None]') StatusName,
+    COUNT(t.TaskId) TaskCount
+FROM Task t
+LEFT OUTER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+GROUP BY s.Name
+ORDER BY s.Name;
+
+-- Should not work.
+SELECT
+    IFNULL(s.Name, '[None]') StatusName,
+    s.IsResolved,
+    COUNT(t.TaskId) TaskCount
+FROM Task t
+LEFT OUTER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+GROUP BY s.Name
+ORDER BY s.Name;
+
+SELECT
+    IFNULL(s.Name, '[None]') StatusName,
+    IFNULL(s.IsResolved, 0) IsResolved,
+    COUNT(t.TaskId) TaskCount
+FROM Task t
+LEFT OUTER JOIN TaskStatus s ON t.TaskStatusId = s.TaskStatusId
+GROUP BY s.Name, s.IsResolved -- IsResolved is now part of the GROUP.
+ORDER BY s.Name;
+
+SELECT
+    CONCAT(w.FirstName, ' ', w.LastName) WorkerName,
+    SUM(t.EstimatedHours) TotalHours
+FROM Worker w
+INNER JOIN ProjectWorker pw ON w.WorkerId = pw.WorkerId
+INNER JOIN Task t ON pw.WorkerId = t.WorkerId
+    AND pw.ProjectId = t.ProjectId
+GROUP BY w.WorkerId, w.FirstName, w.LastName
+HAVING SUM(t.EstimatedHours) >= 100
+ORDER BY SUM(t.EstimatedHours) DESC;
+
+SELECT
+    p.Name ProjectName,
+    MIN(t.DueDate) MinTaskDueDate
+FROM Project p
+INNER JOIN Task t ON p.ProjectId = t.ProjectId
+WHERE p.ProjectId LIKE 'game-%'
+    AND t.ParentTaskId IS NOT NULL
+GROUP BY p.ProjectId, p.Name
+ORDER BY p.Name;
+
+SELECT
+    p.Name ProjectName,
+    MIN(t.DueDate) MinTaskDueDate,
+    MAX(t.DueDate) MaxTaskDueDate,
+    SUM(t.EstimatedHours) TotalHours,
+    AVG(t.EstimatedHours) AverageTaskHours,
+    COUNT(t.TaskId) TaskCount
+FROM Project p
+INNER JOIN Task t ON p.ProjectId = t.ProjectId
+WHERE t.ParentTaskId IS NOT NULL
+GROUP BY p.ProjectId, p.Name
+HAVING COUNT(t.TaskId) >= 10
+ORDER BY COUNT(t.TaskId) DESC, p.Name;
+
+-- This doesn't do what we want.
+SELECT
+    p.Name ProjectName,
+    MIN(t.TaskId) MinTaskId
+    -- t.Title is what we want, but the SQL Engine 
+    -- doesn't know which Task we're talking about!
+    -- t.Title is not part of a group and there's 
+    -- no aggregate guaranteed to grab the Title from the MinTaskId.
+FROM Project p
+INNER JOIN Task t ON p.ProjectId = t.ProjectId
+GROUP BY p.ProjectId, p.Name;
+
+SELECT
+    g.ProjectName,
+    g.MinTaskId,
+    t.Title MinTaskTitle
+FROM Task t
+INNER JOIN (
+    SELECT
+        p.Name ProjectName,
+        MIN(t.TaskId) MinTaskId
+    FROM Project p
+    INNER JOIN Task t ON p.ProjectId = t.ProjectId
+    GROUP BY p.ProjectId, p.Name) g ON t.TaskId = g.MinTaskId;
+    
+    SELECT
+    w.FirstName,
+    w.LastName,
+    (SELECT COUNT(*) FROM ProjectWorker 
+    WHERE WorkerId = w.WorkerId) ProjectCount
+FROM Worker w;
+
+SELECT
+    p.Name ProjectName,
+    MIN(t.TaskId) MinTaskId,
+    (SELECT Title FROM Task 
+    WHERE TaskId = MIN(t.TaskId)) MinTaskTitle
+FROM Project p
+INNER JOIN Task t ON p.ProjectId = t.ProjectId
+GROUP BY p.ProjectId, p.Name
