c51752872091f10559a775e27ab87cd67e4f4886
Melanie Carroll <melanietcarroll@gmail.com>
2020-12-19 04:58:17 -0600

    Hotel tables script

---
+++ Summatives/HotelReservation/MelanieCarroll-HotelData.sql
@@ -0,0 +1,5 @@
+USE HotelReservation;
+
+INSERT INTO RoomType (RoomTypeID, RoomSize, RoomBasePrice, ExtraAdultPrice, StandardOccupancy, Maximum Occupancy) VALUES
+    (1, 'Double', 179.99, 10.00, 2, 4);
+    
