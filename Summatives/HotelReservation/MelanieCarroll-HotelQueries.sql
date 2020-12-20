c1c6d78b069f3184fb00220ed7520bd9ea71eaa9
Melanie Carroll <melanietcarroll@gmail.com>
2020-12-20 14:13:14 -0600

    Hotel DB complete

--- Summatives/HotelReservation/MelanieCarroll-HotelQueries.sql
+++ Summatives/HotelReservation/MelanieCarroll-HotelQueries.sql
@@ -61,12 +61,16 @@ SELECT
     Reservation.StartDate,
     RoomReservation.AdultOccupancy,
     RoomReservation.ChildOccupancy,
-    RoomReservation.RoomNumber,
-    Guest.GuestID
-FROM Guest
-INNER JOIN Reservation ON Guest.GuestID = Reservation.GuestID
-INNER JOIN RoomReservation ON Reservation.ReservationID = RoomReservation.ReservationID
-WHERE GuestID = 1;
+    RoomReservation.RoomNumber
+FROM RoomReservation
+LEFT OUTER JOIN Reservation ON RoomReservation.ReservationID = Reservation.ReservationID
+LEFT OUTER JOIN Guest ON Reservation.GuestID = Guest.GuestID
+WHERE Guest.LastName = 'Carroll';
+
+-- FirstName	LastName	StartDate	AdultOccupancy	ChildOccupancy	RoomNumber	
+-- Melanie	    Carroll	        2023-03-17	       1	         1	         307	      
+-- Melanie	    Carroll	        2023-06-28	       2	         0	         205	      
+
 
 
 
@@ -82,32 +86,32 @@ LEFT OUTER JOIN RoomReservation ON Room.RoomNumber = RoomReservation.RoomNumber
 ORDER BY Room.RoomNumber;
 
 -- RoomNumber	ReservationID	TotalRoomCost
--- 201	4	199.99
--- 202	7	349.98
--- 203	2	999.95
--- 203	20	399.98
--- 204	15	184.99
--- 205	14	699.96
--- 206	12	599.96
--- 206	22	449.97
--- 207	10	174.99
--- 208	12	599.96
--- 208	19	149.99
--- 301	9	799.96
--- 301	22	599.97
--- 302	6	924.95
--- 302	23	699.96
--- 303	17	199.99
--- 304	13	184.99
--- 305	3	349.98
--- 305	18	349.98
--- 306	NULL	NULL
--- 307	5	524.97
--- 308	1	299.98
--- 401	11	1199.97
--- 401	16	1259.97
--- 401	21	1199.97
--- 402	NULL	NULL
+-- 201	               4	     199.99
+-- 202	               7	     349.98
+-- 203	               2	     999.95
+-- 203	               20	     399.98
+-- 204	               15	     184.99
+-- 205	               14	     699.96
+-- 206	               12	     599.96
+-- 206	               22	     449.97
+-- 207	               10	     174.99
+-- 208	               12	     599.96
+-- 208	               19	     149.99
+-- 301	               9	     799.96
+-- 301	               22	     599.97
+-- 302	                6	     924.95
+-- 302	               23	     699.96
+-- 303	               17	     199.99
+-- 304	               13	     184.99
+-- 305	                3	     349.98
+-- 305	                18	     349.98
+-- 306	               NULL	     NULL
+-- 307	                5	     524.97
+-- 308	                1	     299.98
+-- 401	                11	     1199.97
+-- 401	                16	     1259.97
+-- 401	                21	     1199.97
+-- 402	                NULL	NULL
 
 
 -- 5. Write a query that returns all the rooms accommodating at least three guests and that are reserved on any date 
@@ -125,30 +129,30 @@ WHERE (RoomReservation.AdultOccupancy + RoomReservation.ChildOccupancy >= 3)
 AND (Reservation.StartDate BETWEEN '2023-04-01' AND '2023-04-30') ;
 
 -- RoomNumber	AdultOccupancy	ChildOccupancy	StartDate	EndDate
--- 201	2	2	4/9/2023	4/13/2023
--- 201	2	2	4/23/2023	4/24/2023
--- 202	2	2	4/9/2023	4/13/2023
--- 202	2	2	4/23/2023	4/24/2023
--- 203	2	1	4/9/2023	4/13/2023
--- 203	2	1	4/23/2023	4/24/2023
--- 203	2	2	4/9/2023	4/13/2023
--- 203	2	2	4/23/2023	4/24/2023
--- 204	3	1	4/9/2023	4/13/2023
--- 204	3	1	4/23/2023	4/24/2023
--- 301	2	2	4/9/2023	4/13/2023
--- 301	2	2	4/23/2023	4/24/2023
--- 302	3	0	4/9/2023	4/13/2023
--- 302	3	0	4/23/2023	4/24/2023
--- 303	2	1	4/9/2023	4/13/2023
--- 303	2	1	4/23/2023	4/24/2023
--- 304	3	0	4/9/2023	4/13/2023
--- 304	3	0	4/23/2023	4/24/2023
--- 401	2	4	4/9/2023	4/13/2023
--- 401	2	4	4/23/2023	4/24/2023
--- 401	4	2	4/9/2023	4/13/2023
--- 401	4	2	4/23/2023	4/24/2023
--- 401	2	2	4/9/2023	4/13/2023
--- 401	2	2	4/23/2023	4/24/2023
+-- 201	              2           	2	        4/9/2023	4/13/2023
+-- 201	              2	            2	        4/23/2023	4/24/2023
+-- 202	              2	            2	        4/9/2023	4/13/2023
+-- 202	              2	            2	        4/23/2023	4/24/2023
+-- 203	              2	            1	        4/9/2023	4/13/2023
+-- 203	              2	            1	        4/23/2023	4/24/2023
+-- 203	              2	            2	        4/9/2023	4/13/2023
+-- 203	              2	            2	        4/23/2023	4/24/2023
+-- 204	              3	            1	        4/9/2023	4/13/2023
+-- 204	              3             1        	4/23/2023	4/24/2023
+-- 301	              2	            2	        4/9/2023	4/13/2023
+-- 301	              2	            2	        4/23/2023	4/24/2023
+-- 302	              3	            0	        4/9/2023	4/13/2023
+-- 302	              3	            0	        4/23/2023	4/24/2023
+-- 303	              2	            1	        4/9/2023	4/13/2023
+-- 303	              2	            1	        4/23/2023	4/24/2023
+-- 304	              3	            0	        4/9/2023	4/13/2023
+-- 304	              3	            0	        4/23/2023	4/24/2023
+-- 401	              2	            4	        4/9/2023	4/13/2023
+-- 401	              2          	4	        4/23/2023	4/24/2023
+-- 401	              4	            2	        4/9/2023	4/13/2023
+-- 401	              4	            2	        4/23/2023	4/24/2023
+-- 401	              2          	2	        4/9/2023	4/13/2023
+-- 401	              2          	2	        4/23/2023	4/24/2023
 
 
 -- 6. Write a query that returns a list of all guest names and the number of reservations per guest, sorted starting
@@ -165,17 +169,17 @@ GROUP BY Guest.GuestID
 ORDER BY COUNT(Reservation.ReservationID) DESC, Guest.LastName;
 
 --  FirstName	LastName	ReservationCount
--- BettyAnn	Seery	3
--- Mack	Simmer	3
--- Melanie	Carroll	2
--- Duane	Cullison	2
--- Walter	Holaway	2
--- Aurore	Lipton	2
--- Maritza	Tilton	2
--- Wilfred	Vise	2
--- Karie	Yang	2
--- Zachary	Luechtefeld	1
--- Joleen	Tison	1
+-- BettyAnn	    Seery	     3
+-- Mack	        Simmer	     3
+-- Melanie	    Carroll	     2
+-- Duane	    Cullison	 2
+-- Walter	    Holaway	     2
+-- Aurore	    Lipton	     2
+-- Maritza	    Tilton	     2
+-- Wilfred	    Vise	     2
+-- Karie	    Yang	     2
+-- Zachary	    Luechtefeld	 1
+-- Joleen	    Tison	     1
 
 
 -- 7. Write a query that displays the name, address, and phone number of a guest based on their phone number. 
