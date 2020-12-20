12cb4346b754335442109020140e9d3f8c90355a
Melanie Carroll <melanietcarroll@gmail.com>
2020-12-20 11:53:27 -0600

    Hotel DB

--- Summatives/HotelReservation/MelanieCarroll-HotelQueries.sql
+++ Summatives/HotelReservation/MelanieCarroll-HotelQueries.sql
@@ -3,11 +3,39 @@ USE HotelReservation;
 -- 1. Write a query that returns a list of reservations that end in July 2023, including the name of the guest, 
 -- the room number(s), and the reservation dates.
 
+SELECT
+	Guest.FirstName,
+    Guest.LastName,
+    Reservation.StartDate,
+    Reservation.EndDate,
+    RoomReservation.RoomNumber
+FROM Guest
+INNER JOIN Reservation ON Guest.GuestID = Reservation.GuestID
+INNER JOIN RoomReservation ON Reservation.ReservationID = RoomReservation.ReservationID
+WHERE Reservation.EndDate BETWEEN '2023-07-01' AND '2023-07-31';
 
+-- FirstName LastName StartDate	EndDate	   RoomNumber
+-- Melanie	Carroll	2023-06-28	2023-07-02	205
+-- Walter	Holaway	2023-07-13	2023-07-14	204
+-- Wilfred	Vise	2023-07-18	2023-07-21	401
+-- BettyAnn	Seery	2023-07-28	2023-07-29	303
 
 
 -- 2. Write a query that returns a list of all reservations for rooms with a jacuzzi, displaying the guest's name,
 -- the room number, and the dates of the reservation.
+
+SELECT
+	Guest.FirstName,
+    Guest.LastName,
+    Reservation.StartDate,
+    Reservation.EndDate,
+    RoomReservation.RoomNumber
+FROM Guest
+INNER JOIN Reservation ON Guest.GuestID = Reservation.GuestID
+INNER JOIN RoomReservation ON Reservation.ReservationID = RoomReservation.ReservationID
+WHERE Reservation.EndDate BETWEEN '2023-07-01' AND '2023-07-31';
+
+
 
 -- 3. Write a query that returns all the rooms reserved for a specific guest, including the guest's name, the room(s) 
 -- reserved, the starting date of the reservation, and how many people were included in the reservation. 
