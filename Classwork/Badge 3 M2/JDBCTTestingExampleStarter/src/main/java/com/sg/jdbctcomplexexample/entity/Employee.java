257a6e396504534d32d397dd5eb51de92c7aebcd
melanietcarroll <melanietcarroll@gmail.com>
2020-12-26 20:32:02 -0600

    more Badge 3 M2 coursework

---
+++ Classwork/Badge 3 M2/JDBCTTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/entity/Employee.java
@@ -0,0 +1,61 @@
+package com.sg.jdbctcomplexexample.entity;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author kylerudy
+ */
+public class Employee {
+    int id;
+    String firstName;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.id;
+        hash = 83 * hash + Objects.hashCode(this.firstName);
+        hash = 83 * hash + Objects.hashCode(this.lastName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Employee other = (Employee) obj;
+        return true;
+    }
+    String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ Classwork/Badge 3 M2/JDBCTTestingExampleStarter/src/main/java/com/sg/jdbctcomplexexample/entity/Meeting.java
@@ -0,0 +1,86 @@
+package com.sg.jdbctcomplexexample.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author kylerudy
+ */
+public class Meeting {
+    int id;
+    String name;
+    LocalDateTime time;
+    Room room;
+    List<Employee> attendees = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.id;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.time);
+        hash = 67 * hash + Objects.hashCode(this.room);
+        hash = 67 * hash + Objects.hashCode(this.attendees);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Meeting other = (Meeting) obj;
+        return true;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public List<Employee> getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(List<Employee> attendees) {
+        this.attendees = attendees;
+    }
+    
+    
+}
