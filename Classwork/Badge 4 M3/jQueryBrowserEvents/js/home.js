44f0822ff58708a42c76dc8cdcbae5babdd73d83
melanietcarroll <melanietcarroll@gmail.com>
2021-01-11 21:18:47 -0600

    badge 4 m3 classwork

--- Classwork/Badge 4 M3/jQueryBrowserEvents/js/home.js
+++ Classwork/Badge 4 M3/jQueryBrowserEvents/js/home.js
@@ -3,41 +3,51 @@ $(document).ready(function () {
     $('#akronInfoDiv').hide();
     $('#minneapolisInfoDiv').hide();
     $('#louisvilleInfoDiv').hide();
-    $('#akronButton').on('click', function() {
-       $('#akronInfoDiv').toggle('slow'); 
-       $('#akronWeather').hide();
-       $('#minneapolisInfoDiv').hide();
+    $('#akronButton').on('click', function () {
+        $('#akronInfoDiv').toggle('slow');
+        $('#akronWeather').hide();
+        $('#minneapolisInfoDiv').hide();
         $('#louisvilleInfoDiv').hide();
     });
-    $('#minneapolisButton').on('click', function() {
-       $('#minneapolisInfoDiv').toggle('slow'); 
-       $('#minneapolisWeather').hide();
+    $('#minneapolisButton').on('click', function () {
+        $('#minneapolisInfoDiv').toggle('slow');
+        $('#minneapolisWeather').hide();
         $('#louisvilleInfoDiv').hide();
         $('#akronInfoDiv').hide();
     });
-    $('#louisvilleButton').on('click', function() {
-       $('#louisvilleInfoDiv').toggle('slow'); 
-       $('#louisvilleWeather').hide();
+    $('#louisvilleButton').on('click', function () {
+        $('#louisvilleInfoDiv').toggle('slow');
+        $('#louisvilleWeather').hide();
         $('#minneapolisInfoDiv').hide();
         $('#akronInfoDiv').hide();
     });
-    $('#akronWeatherButton').on('click', function() {
-       $('#akronWeather').toggle('slow'); 
+    $('#akronWeatherButton').on('click', function () {
+        $('#akronWeather').toggle('slow');
     });
-    $('#minneapolisWeatherButton').on('click', function() {
-       $('#minneapolisWeather').toggle('slow'); 
+    $('#minneapolisWeatherButton').on('click', function () {
+        $('#minneapolisWeather').toggle('slow');
     });
-    $('#louisvilleWeatherButton').on('click', function() {
-       $('#louisvilleWeather').toggle('slow'); 
+    $('#louisvilleWeatherButton').on('click', function () {
+        $('#louisvilleWeather').toggle('slow');
     });
-    $("td").hover(
+    // $("td").hover(
     // in callback
-    function () {
-        $(this).css("background-color", "WhiteSmoke");
-    },
+    // function () {
+    //     $(this).css("background-color", "WhiteSmoke");
+    // },
     // out callback
-    function () {
-        $(this).css("background-color", "");
-    }
-);
+    //function () {
+    //    $(this).css("background-color", "");
+    //}
+//);
+    $("tr").not(':first').hover(
+            function () {
+                $(this).css("background-color", "WhiteSmoke");
+            },
+            function () {
+                $(this).css("background-color", "");
+            }
+    );
+    
+    
 });
